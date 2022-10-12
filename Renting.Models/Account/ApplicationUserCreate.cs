d371ace2cc232e813e41d9355d42ce96495ccc3b
Ebru Şara Bağca <ebru.sara123@outlook.com>
2022-10-12 19:14:04 +0300

    ApplicationUserCreate.cs and ApplicationUserLogin had been updated (validation)

--- Renting.Models/Account/ApplicationUserCreate.cs
+++ Renting.Models/Account/ApplicationUserCreate.cs
@@ -4,14 +4,20 @@ namespace Renting.Models.Account;
 
 public class ApplicationUserCreate : ApplicationUserLogin
 {
-
+    [Required(ErrorMessage = "Firstname is required")]
+    [MaxLength(20, ErrorMessage = "Must be at most 20 characters")]
     public string? FirstName { get; set; }
-    
+
+    [Required(ErrorMessage = "Lastname is required")]
+    [MaxLength(20, ErrorMessage = "Must be at most 20 characters")]
     public string? LastName { get; set; }
-    
+
+    [Required(ErrorMessage = "Username is required")]
+    [MinLength(5, ErrorMessage = "Must be at least 5 characters")]
+    [MaxLength(20, ErrorMessage = "Must be at most 20 characters")]
     public string Username { get; set; }
 
+    [Required(ErrorMessage = "Gender is required")]
+    [MaxLength(20, ErrorMessage = "Must be at most 20 characters")]
     public string Gender { get; set; }
-
-    public int? PhotoId { get; set; }
 }
--- Renting.Models/Account/ApplicationUserLogin.cs
+++ Renting.Models/Account/ApplicationUserLogin.cs
@@ -4,7 +4,12 @@ namespace Renting.Models.Account;
 
 public class ApplicationUserLogin
 {
-    
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
     public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+    [MaxLength(15, ErrorMessage = "Password must be at most 15 characters")]
     public string Password { get; set; }
 }
